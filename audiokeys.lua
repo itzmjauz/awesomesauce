68f68f59e766997832554d7c0240721ca1721001
itzmjauz <itmjauz@gmail.com>
2012-12-04 13:00:09 +0100

    added output when changing sound volume

--- audiokeys.lua
+++ audiokeys.lua
@@ -1,6 +1,14 @@
 globalkeys = awful.util.table.join(globalkeys,
-    awful.key({ }, "XF86AudioMute",function () awful.util.spawn('amixer set Master togglemute') end),
-    awful.key({ }, "XF86Tools",function () awful.util.spawn('amixer set Master unmute') end),
-    awful.key({ }, "XF86AudioRaiseVolume",function () awful.util.spawn('amixer set Master 10%+') end),
-    awful.key({ }, "XF86AudioLowerVolume",function () awful.util.spawn('amixer set Master 10%-') end)
+    awful.key({ }, "XF86AudioMute",function () 
+        say(awful.util.pread('amixer set Master togglemute'))
+    end),
+    awful.key({ }, "XF86Tools",function () 
+        say(awful.util.pread('amixer set Master unmute')) 
+    end),
+    awful.key({ }, "XF86AudioRaiseVolume",function () 
+        say(awful.util.pread('amixer set Master 10%+')) 
+    end),
+    awful.key({ }, "XF86AudioLowerVolume",function () 
+        say(awful.util.pread('amixer set Master 10%-'))
+    end)
 )
