55df6a2a11508787b535e02976f61b4c186c8ae5
Nathan Zadoks <nathan@nathan7.eu>
2013-01-23 14:27:44 +0100

    add task keybindings

--- bindings/global.lua
+++ bindings/global.lua
@@ -7,6 +7,7 @@ local keys = awful.util.table.join({}
   , awful.key({ modkey, 'Control' }, 'r', awesome.restart)
   , awful.key({ modkey, 'Shift'   }, 'q', awesome.quit)
   , require('bindings.global.tags').keys
+  , require('bindings.global.tasks').keys
   , require('bindings.global.layout').keys
   , require('bindings.global.programs').keys
   , require('bindings.global.prompt').keys
