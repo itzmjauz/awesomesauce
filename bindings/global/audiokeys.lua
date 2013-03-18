5d27fdb6ac343d67fdcb998ce372225abd67247d
Antoni Stevenet <itzmjauz@gmail.com>
2013-03-18 13:40:51 +0100

    notifications when changing volume/muting sound added

--- bindings/global/audiokeys.lua
+++ bindings/global/audiokeys.lua
@@ -1,10 +1,13 @@
 local awful = require('awful')
+local naughty = require('naughty')
+
+function say(text) naughty.notify({ text = text }) end
+function sayShell(cmd) say(awful.util.pread(cmd)) end
 
 local keys = awful.util.table.join({}
-  ,  awful.key({ }, "XF86AudioMute",       function () awful.util.spawn('amixer set Master togglemute') end)
-  ,  awful.key({ }, "XF86Tools",           function () awful.util.spawn('amixer set Master unmute') end)
-  ,  awful.key({ }, "XF86AudioRaiseVolume",function () awful.util.spawn('amixer set Master 10%+') end)
-  ,  awful.key({ }, "XF86AudioLowerVolume",function () awful.util.spawn('amixer set Master 10%-') end)
-  )
-
+  ,    awful.key({ }, "XF86AudioMute",         function () sayShell('amixer set Master togglemute') end)
+  ,    awful.key({ }, "XF86Tools",             function () sayShell('amixer set Master unmute') end)
+  ,    awful.key({ }, "XF86AudioRaiseVolume",  function () sayShell('amixer set Master 10%+')  end)
+  ,    awful.key({ }, "XF86AudioLowerVolume",  function () sayShell('amixer set Master 10%-') end)
+)
 return { keys = keys }
