85d09e564af849b938d3850b5f90dd31fee667ff
Antoni Stevenet <itzmjauz@gmail.com>
2018-06-11 15:02:24 +0200

    add background, remove orphan files

--- wallpaper.lua
+++
@@ -1,7 +0,0 @@
-local wallpaper = require('beautiful').wallpaper
-if not wallpaper then return end
-local gears = require('gears')
-
-for s = 1, screen.count() do
-    gears.wallpaper.maximized(wallpaper, s, true)
-end
--- wiboxes/tasklists.lua
+++
@@ -1,9 +0,0 @@
-local awful = require('awful')
-local buttons = require('bindings.global.tasks').buttons
-
-local tasklists = {}
-for s = 1, screen.count() do
-  tasklists[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, buttons)
-end
-
-return tasklists
